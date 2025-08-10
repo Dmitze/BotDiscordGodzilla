28ff5bb456556c06975be3db0f338e081242c964
Дмитрий Шивачов <102044731+Dmitze@users.noreply.github.com>
2025-08-11 01:17:42 +0300

    chore: gitignore dist and remove tracked build artifacts

--- dist/types/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Основні типи для Discord AI Assistant Bot
-// Версія 3.0.0 - Повністю рефакторовано з детальним логуванням
-Object.defineProperty(exports, "__esModule", { value: true });
-// Всі типи експортовані безпосередньо з цього файлу 
-//# sourceMappingURL=index.js.map
